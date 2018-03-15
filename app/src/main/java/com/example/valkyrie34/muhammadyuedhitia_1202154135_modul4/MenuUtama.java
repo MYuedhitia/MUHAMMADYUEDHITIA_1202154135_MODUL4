751e289560da61f1c3b741aca59d9bf517fff66b
unknown <vforimmortaly@gmail.com>
2018-03-16 01:52:43 +0700

    first commit

---
+++ app/src/main/java/com/example/valkyrie34/muhammadyuedhitia_1202154135_modul4/MenuUtama.java
@@ -0,0 +1,24 @@
+package com.example.valkyrie34.muhammadyuedhitia_1202154135_modul4;
+
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MenuUtama extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_utama);
+    }
+
+    public void tampilNama(View view) {
+        Intent i = new Intent(getApplicationContext(),TampilNama.class);
+        startActivity(i);
+    }
+
+    public void tampilGambar(View view) {
+    }
+}
